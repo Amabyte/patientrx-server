294a1d188eeed806d64c8c81945d9fd557867dfd
Vishal Vijay <0vishalvijay0@gmail.com>
2014-06-05 01:16:06 +0530

    profile api

--- app/controllers/api/v1/patient_account_controller.rb
+++ app/controllers/api/v1/patient_account_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::PatientAccountController < ApplicationController
-  before_action :authenticate_patient!
+  before_action :authenticate_api_patient!
 
   respond_to :json
   
   def profile
-    respond_with current_patient
+    respond_with current_api_patient
   end
 end
