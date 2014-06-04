f5116e74c4f7a84d41ba57d65e1b9f4b9cffa4c8
Vishal Vijay <0vishalvijay0@gmail.com>
2014-06-05 03:01:30 +0530

    sociallogin, signout and profilw

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Patientrx::Application.routes.draw do
   api vendor_string: "patientrx", default_version: 1 do
     version 1 do
       cache as: 'v1' do
-        devise_for :patients, :skip => [:registration]
+        devise_for :patients, :skip => [:registration], controllers: { sessions: "api/v1/patient_session" }
         devise_scope :patient do
           resources :patients, controller: "patient_account", :only =>[] do
             collection do
-              post '/social_login', to: 'patient_session#social_login'
+              post '/social_login', to: :social_login
               get "/profile", to: :profile
             end
           end
